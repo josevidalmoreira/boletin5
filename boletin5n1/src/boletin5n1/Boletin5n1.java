529025d6d7f7fe3ae84a0903c2fd9ceca2bd68ed
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-11-08 09:19:48 +0100

    inicio 2

---
+++ boletin5n1/src/boletin5n1/Boletin5n1.java
@@ -0,0 +1,19 @@
+
+package boletin5n1;
+import java.util.Scanner;
+
+public class Boletin5n1 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float v;
+       System.out.println("teclea numero ");
+       v = sc.nextFloat();
+       
+       if(v>=0){
+           System.out.println("positivo");
+       }
+    }
+    
+}
